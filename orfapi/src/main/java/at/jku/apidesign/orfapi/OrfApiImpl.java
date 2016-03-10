36095d6cd4c0a944afe9bf35c9a24834b803ec16
dpril <d.prilmueller@gmx.at>
2016-03-10 13:12:17 +0100

    .

--- orfapi/src/main/java/at/jku/apidesign/orfapi/OrfApiImpl.java
+++ orfapi/src/main/java/at/jku/apidesign/orfapi/OrfApiImpl.java
@@ -1,18 +1,57 @@
 package at.jku.apidesign.orfapi;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import at.jku.apidesign.orfapi.exception.OrfApiException;
 import at.jku.apidesign.orfapi.model.Category;
 import at.jku.apidesign.orfapi.model.NewsArticle;
 import at.jku.apidesign.orfapi.model.Region;
 
-public class OrfApiImpl implements OrfApi{
+public final class OrfApiImpl implements OrfApi {
+
+	private String getHeader(Element element, String tagName) {
+		String header = null;
+		Elements headerElement = element.getElementsByTag(tagName);
+		if (!headerElement.isEmpty()) {
+			header = headerElement.get(0).text();
+		}
+		return header;
+	}
 
 	@Override
-	public List<NewsArticle> getTopNews() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<NewsArticle> getTopNews() throws OrfApiException {
+		List<NewsArticle> topNews = new ArrayList<NewsArticle>();
+		Document orfDocument;
+		try {
+			orfDocument = WebDocument.getJSoupDocument("http://news.orf.at");
+		} catch (IOException ex) {
+			throw new OrfApiException(ex);
+		}
+		for (Element ressort : orfDocument.select("main .ticker .ressort")) {
+			String topic = getHeader(ressort, "h1");
+
+			System.out.println(topic);
+			System.out.println();
+
+			for (Element article : ressort.select(".stories article")) {
+				String title = getHeader(article, "h2");
+				System.out.println(title);
+
+				String text = article.select(".text").get(0).text();
+				System.out.println(text);
+			}
+
+			System.out.println("------");
+			System.out.println();
+		}
+		return topNews;
 	}
 
 	@Override
