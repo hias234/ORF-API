39d62ae67edc36cffb2a9197c94a0371322dcd1e
dpril <d.prilmueller@gmx.at>
2016-03-10 16:27:10 +0100

    Console Application

--- orfapi/src/main/java/at/jku/apidesign/orfapi/console/OrfApiProgram.java
+++ orfapi/src/main/java/at/jku/apidesign/orfapi/console/OrfApiProgram.java
@@ -1,8 +1,70 @@
 package at.jku.apidesign.orfapi.console;
 
-public class OrfApiProgram {
+import java.util.List;
+import java.util.Scanner;
 
-	public static void main() {
-		System.out.println("Hello");
+import at.jku.apidesign.orfapi.OrfApi;
+import at.jku.apidesign.orfapi.OrfApiImpl;
+import at.jku.apidesign.orfapi.model.Category;
+import at.jku.apidesign.orfapi.model.NewsArticle;
+
+public class OrfApiProgram {
+	private static final Scanner s = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the ORF-News API (exit...x):");
+		System.out.println("(1)...getTopNews");
+		System.out.println("(2)...getTopNewsByCategory");
+		System.out.println("(3)...searchTopNews");
+		System.out.println("(4)...getNewsByRegion");
+		System.out.println("(5)...searchNewsByRegion");
+		System.out.println("(6)...getNewsByRegionAndDate");
+
+		do {
+			if (s.hasNextInt()) {
+				int selection = s.nextInt();
+				callApiMethod(selection);
+			}
+		} while (!s.nextLine().toLowerCase().equals("x"));
+
+		System.out.println("Application closed!");
+	}
+
+	private static void callApiMethod(int selection) {
+		OrfApi orfApi = new OrfApiImpl();
+		switch (selection) {
+		case 1:
+			printNews(orfApi.getTopNews());
+			break;
+		case 2:
+			orfApi.getTopNewsByCategory(enterCategory());
+			break;
+		default:
+			System.out.println("No method mapped to your input.");
+			break;
+		}
+	}
+
+	private static void printNews(List<NewsArticle> news) {
+		// TODO print whole news
+		// Besser schreiben in file (html, text...)
+		for (NewsArticle article : news) {
+			System.out.println(article.getTitle());
+		}
+	}
+
+	private static Category enterCategory() {
+		System.out.println("Category:");
+
+		for (int i = 0; i < Category.values().length; i++) {
+			if (i > 0)
+				System.out.print(", ");
+			System.out.print(Category.values()[i].name() + "(" + (i + 1) + ")");
+		}
+		if (s.hasNextInt()) {
+			int catIdx = s.nextInt();
+			return Category.values()[catIdx];
+		}
+		return null;
 	}
 }
