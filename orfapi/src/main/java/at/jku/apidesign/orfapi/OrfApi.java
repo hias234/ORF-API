115aa009b8b5f08155da68500d49c13c6a0573af
dpril <d.prilmueller@gmx.at>
2016-03-10 13:13:28 +0100

    OrfApiException

--- orfapi/src/main/java/at/jku/apidesign/orfapi/OrfApi.java
+++ orfapi/src/main/java/at/jku/apidesign/orfapi/OrfApi.java
@@ -3,6 +3,7 @@ package at.jku.apidesign.orfapi;
 import java.util.Date;
 import java.util.List;
 
+import at.jku.apidesign.orfapi.exception.OrfApiException;
 import at.jku.apidesign.orfapi.model.Category;
 import at.jku.apidesign.orfapi.model.NewsArticle;
 import at.jku.apidesign.orfapi.model.Region;
@@ -15,40 +16,39 @@ public interface OrfApi {
 	/**
 	 * @return
 	 */
-	public List<NewsArticle> getTopNews();
-	
+	public List<NewsArticle> getTopNews() throws OrfApiException;
+
 	/**
 	 * @param category
 	 * @return
 	 */
-	public List<NewsArticle> getTopNewsByCategory(Category category);
-	
+	public List<NewsArticle> getTopNewsByCategory(Category category) throws OrfApiException;
+
 	/**
 	 * @param query
 	 * @return
 	 */
-	public List<NewsArticle> searchTopNews(String query);
-	
+	public List<NewsArticle> searchTopNews(String query) throws OrfApiException;
+
 	/**
 	 * @param region
 	 * @return
 	 */
-	public List<NewsArticle> getNewsByRegion(Region region);
-	
+	public List<NewsArticle> getNewsByRegion(Region region) throws OrfApiException;
+
 	/**
 	 * @param region
 	 * @param query
 	 * @return
 	 */
-	public List<NewsArticle> searchNewsByRegion(Region region, String query);
-	
+	public List<NewsArticle> searchNewsByRegion(Region region, String query) throws OrfApiException;
+
 	/**
 	 * @param region
 	 * @param from
 	 * @param to
 	 * @return
 	 */
-	public List<NewsArticle> getNewsByRegionAndDate(Region region, Date from, Date to);
+	public List<NewsArticle> getNewsByRegionAndDate(Region region, Date from, Date to) throws OrfApiException;
 
-	
 }
